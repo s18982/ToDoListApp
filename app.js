0a5a8a71ba5c721e15917df74a02e8e776cccd83
s18982@pjwstk.edu.pl <s18982@pjwstk.edu.pl>
2022-08-03 12:35:56 +0200

    selectors, eventListeners, decl funcs

---
+++ app.js
@@ -0,0 +1,25 @@
+// selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoFilter = document.querySelector('.filter-todo');
+const todoList = document.querySelector('.todo-list');
+
+// event listeners
+todoButton.addEventListener('click',addTodo);
+todoList.addEventListener('click',deleteChceck);
+todoFilter.addEventListener('click',filterTodo);
+
+// adding
+function addTodo(event){
+
+}
+
+//
+function deleteChceck(e){
+
+}
+
+//
+function filterTodo(e){
+
+}
